87e5a63ee3db311ef740f0727c960d07103b3166
Kevin Harkins <kevin.harkins@vanderbilt.edu>
2017-08-28 11:14:08 -0500

    fixed diffusion bugs. Added custom Tukey window function that doesn't require an additional matlab toolbox.

--- +remmi/+vendors/bmatBruker.m
+++ +remmi/+vendors/bmatBruker.m
@@ -46,54 +46,56 @@ bigD = pp.methpars.PVM_DwGradSep; % ms
 litD = pp.methpars.PVM_DwGradDur; % ms
 
 for n=1:length(dro)
-    t = 0:dt:(te+tm);
-    Gr = zeros(size(t));
-    Gp = zeros(size(t));
-    Gs = zeros(size(t));
+    for p=1:length(gdiff)
+        t = 0:dt:(te+tm);
+        Gr = zeros(size(t));
+        Gp = zeros(size(t));
+        Gs = zeros(size(t));
 
-    % 180 slice select and crusher
-    mask = (t>te/2-tssr/2-tcrush) & (t<=te/2);
-    Gs(mask) = gcrush;
-    mask = (t>te/2-tssr/2) & (t<=te/2);
-    Gs(mask) = gssr;
+        % 180 slice select and crusher
+        mask = (t>te/2-tssr/2-tcrush) & (t<=te/2);
+        Gs(mask) = gcrush;
+        mask = (t>te/2-tssr/2) & (t<=te/2);
+        Gs(mask) = gssr;
 
-    mask = (t<te/2+tm+tssr/2+tcrush) & (t>=te/2+tm);
-    Gs(mask) = -gcrush;
-    mask = (t<te/2+tm+tssr/2) & (t>=te/2+tm);
-    Gs(mask) = -gssr;
+        mask = (t<te/2+tm+tssr/2+tcrush) & (t>=te/2+tm);
+        Gs(mask) = -gcrush;
+        mask = (t<te/2+tm+tssr/2) & (t>=te/2+tm);
+        Gs(mask) = -gssr;
 
-    % read out direction
-    mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
-    Gr(mask) = gdiff*dro(n);
-    mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
-    Gr(mask) = -gdiff*dro(n);
-    
-    % phase encode direction
-    mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
-    Gp(mask) = +gdiff*dpe(n);
-    mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
-    Gp(mask) = -gdiff*dpe(n);
-    
-    % slice select direction
-    mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
-    Gs(mask) = gdiff*dsl(n);
-    mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
-    Gs(mask) = -gdiff*dsl(n);
-    
-    G = [Gr;Gp;Gs];
-    
-    plot(G')
-    pause();
-    
-    % calculate the b-matrix
-    k = cumsum(G,2)*dt*gamma*2*pi/1000; % 1/mm
-    b = zeros(3,3);
-    for m=1:size(k,2)
-        b = b + k(:,m)*k(:,m)';
+        % read out direction
+        mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
+        Gr(mask) = gdiff(p)*dro(n);
+        mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
+        Gr(mask) = -gdiff(p)*dro(n);
+
+        % phase encode direction
+        mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
+        Gp(mask) = +gdiff(p)*dpe(n);
+        mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
+        Gp(mask) = -gdiff(p)*dpe(n);
+
+        % slice select direction
+        mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
+        Gs(mask) = gdiff(p)*dsl(n);
+        mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
+        Gs(mask) = -gdiff(p)*dsl(n);
+
+        G = [Gr;Gp;Gs];
+
+        % plot(G')
+        % pause();
+
+        % calculate the b-matrix
+        k = cumsum(G,2)*dt*gamma*2*pi/1000; % 1/mm
+        b = zeros(3,3);
+        for m=1:size(k,2)
+            b = b + k(:,m)*k(:,m)';
+        end
+        %bm(:,:,n) = b*dt/1000; % s/mm^2
+
+        bm(:,(n-1)*length(gdiff)+p) = [diag(b);diag(b,-1);diag(b,-2);1]*dt/1000; % s/mm^2
     end
-    %bm(:,:,n) = b*dt/1000; % s/mm^2
-    
-    bm(:,n) = [diag(b);diag(b,-1);diag(b,-2);1]*dt/1000; % s/mm^2
     
 end
 
--- +remmi/+vendors/loadBruker.m
+++ +remmi/+vendors/loadBruker.m
@@ -66,8 +66,8 @@ else
 end
 
 % if reference phase data exists, read it in
-ph_ref0 = zeros(1,rarefactor,1,1,length(echotimes),nslice,diffImgs);
-ph_ref1 = zeros(1,rarefactor,1,1,length(echotimes),nslice,diffImgs);
+ph_ref0 = zeros(1,rarefactor,1,1,length(echotimes),nslice,diffImgs,1,1,nreps);
+ph_ref1 = zeros(1,rarefactor,1,1,length(echotimes),nslice,diffImgs,1,1,nreps);
 if isfield(methpars,'REMMI_ProcnoResult')
     vals = strsplit(methpars.REMMI_ProcnoResult(2:end-1),',');
     [fstudy,~] = fileparts(dataPath);
@@ -80,15 +80,22 @@ if isfield(methpars,'REMMI_ProcnoResult')
     ph_raw = ph_raw(1,:) + 1i*ph_raw(2,:);
     
     % set format to [readout, echo]
-    ph_raw = reshape(ph_raw,[],rarefactor*length(echotimes),nslice,diffImgs);
+    ph_raw = reshape(ph_raw,[],rarefactor*length(echotimes),nslice,diffImgs,1,1,nreps);
     ph_raw = ph_raw(1:encmatrix(1),:);
     
     [~,idx] = max(abs(ph_raw(:,1)));
     rng = (-1:1) + idx;
     ph_ref0 = angle(sum(ph_raw(rng,:),1));
-    
-    ph_ref0 = reshape(ph_ref0,[1 rarefactor 1 1 length(echotimes) nslice diffImgs]);
     ph_ref1 = zeros(size(ph_ref0));
+    
+    if isfield(methpars,'REMMI_DwiOnOff')
+        if strcmp(methpars.REMMI_DwiOnOff,'Yes')
+            [ph_ref0,ph_ref1] = dwi_phase_corr(ph_raw);
+        end
+    end
+    
+    ph_ref0 = reshape(ph_ref0,[1 rarefactor 1 1 length(echotimes) nslice diffImgs 1 1 nreps]);
+    ph_ref1 = reshape(ph_ref1,[1 rarefactor 1 1 length(echotimes) nslice diffImgs 1 1 nreps]);
 end
 
 
@@ -115,7 +122,7 @@ data = reshape(data,roalign,rarefactor,length(echotimes),nslice,...
 
 % reorder
 data = permute(data,[1,2,5,6,3,4,7:ndims(data)]);
-data = data(1:encmatrix(1),:,:,:,:,:,:,:,:);
+data = remmi.util.slice(data,1,1:encmatrix(1));
 % format is now [ro,rare,pe1,pe2,echoes,slices,diffImgs,irImgs,mtImgs,nreps]
 
 % move to projection space
@@ -123,7 +130,7 @@ proj = fftshift(fft(fftshift(data,1)),1);
 
 % correct for 0th & first order phase
 proj = bsxfun(@times,proj,exp(-1i*(...
-    bsxfun(@plus, ph_ref0(1,:,1), bsxfun(@times,(1:encmatrix(1))',ph_ref1)))));
+    bsxfun(@plus, ph_ref0, bsxfun(@times,(1:encmatrix(1))',ph_ref1)))));
 
 % back into full fourier space
 data = ifftshift(ifft(ifftshift(proj,1)),1);
@@ -131,7 +138,7 @@ data = ifftshift(ifft(ifftshift(proj,1)),1);
 % use phase encode tables
 data = reshape(data,encmatrix(1),encmatrix(2),encmatrix(3),length(echotimes),...
     nslice,diffImgs,irImgs,mtImgs,nreps);
-data(:,pe1table,pe2table,:,:,:,:,:,:,:) = data;
+data(:,pe1table,pe2table,:,:,:,:,:,:,:,:) = data;
 
 % flip odd echoes in gradient echo sequences
 if methpars.PVM_NEchoImages>1 && ~isfield(methpars,'RefPulse1')
@@ -152,7 +159,7 @@ data = bsxfun(@times,data,exp(-1i*2*pi*line*methpars.PVM_Phase1Offset/methpars.P
 % PE2 shift
 if length(methpars.PVM_EncMatrix) > 2
     np = methpars.PVM_EncMatrix(3);
-    line = reshape((1:np) - 1 - round(np/2),1,[]);
+    line = reshape((1:np) - 1 - round(np/2),1,1,[]);
     data = bsxfun(@times,data,exp(-1i*2*pi*line*methpars.PVM_Phase2Offset/methpars.PVM_Fov(3)));
 end
 
@@ -167,4 +174,27 @@ img = fftshift(fftshift(fftshift(img,1),2),3);
 
 img = permute(img(:,end:-1:1,:,:,:,:,:,:,:),[2 1 3 4 5 6 7 8 9]);
 
+end
+
+function [ph0,ph1] = dwi_phase_corr(raw)
+
+proj = fftshift(fft(fftshift(raw,1)),1);
+
+ph0 = zeros(size(raw,2),1);
+ph1 = zeros(size(raw,2),1);
+
+p1 = proj(:,1);
+for n=2:size(proj,2)
+    pn = proj(:,n);
+    mask = abs(pn)/max(abs(pn)) > 0.1;
+    ph_diff = pn./p1;
+    
+    idx = 1:length(pn);
+    phw = unwrap(angle(ph_diff(mask)));
+    ph = remmi.util.linfitfast(idx(mask)',phw,abs(pn(mask)));
+    ph0(n) = ph(2);
+    ph1(n) = ph(1);
+end
+
+end
 
