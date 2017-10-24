71449e24ac205ccc2db86f9570ec8700bbf405c5
Kevin <kevin.harkins@vanderbilt.edu>
2017-10-24 10:30:05 -0500

    by default, no apodization

--- +remmi/+recon/options.m
+++ +remmi/+recon/options.m
@@ -6,13 +6,18 @@ function opts = options(opts)
 %
 %   this function does not overwrite already set options
 %
+%  Kevin Harkins & Mark Does, Vanderbilt University
+%  for the REMMI Toolbox
 
 if ~exist('opts','var')
     opts = struct();
 end
 
 if ~isfield(opts,'apodize_fn') || isempty(opts.apodize_fn)
-    opts.apodize_fn = str2func('@(x)remmi.util.apodize(x,0.25)');
+    % by default, no apodization...?
+    opts.apodize_fn = str2func('@(x) x');
+    
+    %opts.apodize_fn = str2func('@(x)remmi.util.apodize(x,0.25)');
 end
 
 if ~isfield(opts,'matrix_sz')
