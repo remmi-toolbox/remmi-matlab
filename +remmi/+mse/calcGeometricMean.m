6ea31cad8dba31845afb4604201ef0f8bfbbeaf4
Kevin <kevin.harkins@vanderbilt.edu>
2017-10-24 10:23:00 -0500

    add example to help for remmi.mse.calc* functions

--- +remmi/+mse/calcGeometricMean.m
+++ +remmi/+mse/calcGeometricMean.m
@@ -2,13 +2,16 @@ function gmT2 = calcGeometricMean(out)
 % sig = remmi.mse.calcGeometricMean(out) calculates the geometric mean T2
 % from a T2 spectrum
 %
-%   out = output from remmi.mse.MERA.MERA()
+%   out = output from remmi.mse.MERA.MERA(...)
 %
 %   gmT2 = geometric mean T2
 %
+% example:
+%   metrics.gmT2 = @remmi.mse.calcGeometricMean;
+%   epg_struct = remmi.mse.mT2(img_struct,metrics);
+%
 % Kevin Harkins & Mark Does, Vanderbilt University
 % for the REMMI Toolbox
-
 
 lTv = log(out.Tv);
 lTv(isinf(lTv)) = 0;
--- +remmi/+mse/calcT2frac.m
+++ +remmi/+mse/calcT2frac.m
@@ -1,11 +1,15 @@
 function frac = calcT2frac(out,lb,ub)
 % sig = remmi.mse.calcT2frac(out,lb,ub) calculates a T2 signal fraction
 %
-%   out = output from remmi.mse.MERA.MERA()
+%   out = output from remmi.mse.MERA.MERA(...)
 %   lb,ub = lower and upper bounds on T2 peaks to include in the
 %   calculation
 %
 %   frac = fraction of signal exhibiting T2s between lb and ub
+%
+% example:
+%   metrics.MWF = @(out) remmi.mse.calcT2frac(out,0.005,0.020);
+%   epg_struct = remmi.mse.mT2(img_struct,metrics);
 %
 % Kevin Harkins & Mark Does, Vanderbilt University
 % for the REMMI Toolbox
